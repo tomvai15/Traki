502c12eb79108ad3422c594387f63f65252ce2ee
tomvai15 <78442825+tomvai15@users.noreply.github.com>
2023-02-17 20:18:38 +0200

    tests: add controller dependecy tests

---
+++ tests/Traki.UnitTests/Bootstrapping/ControllerDependecyTests.cs
@@ -0,0 +1,47 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Traki.Api;
+
+namespace Traki.UnitTests.Bootstrapping
+{
+    public class ControllerDependecyTests
+    {
+        [Theory]
+        [MemberData(nameof(Controllers))]
+        public void CanResolveControllers(Type type)
+        {
+            // Arrange
+            IConfigurationBuilder configurationBuilder = new ConfigurationBuilder();
+            configurationBuilder.AddJsonFile("appsettings.json");
+            IConfiguration configuration = configurationBuilder.Build();
+
+            IServiceCollection services = new ServiceCollection();
+            Startup.ConfigureServices(services, configuration);
+
+            services.AddSingleton(type);
+
+            IServiceProvider serviceProvider = services.BuildServiceProvider();
+
+            // Act
+            using var scope = serviceProvider.CreateScope();
+            var controller = scope.ServiceProvider.GetService(type);
+
+            // Assert
+            controller.Should().NotBeNull();
+        }
+
+        public static IEnumerable<object[]> Controllers()
+            => Assembly.GetAssembly(typeof(Startup)).GetTypes()
+                 .Where(type => type.IsSubclassOf(typeof(ControllerBase)))
+                 .Select(x => new object[] { x });
+
+    }
+}
