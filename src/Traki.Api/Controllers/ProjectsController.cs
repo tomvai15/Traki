c20cbf65d0852b9d09700d3c1e612c6f58e03f13
tomvai15 <78442825+tomvai15@users.noreply.github.com>
2023-02-25 13:26:03 +0200

    feat: add ProjectController and Handler

---
+++ src/Traki.Api/Controllers/ProjectsController.cs
@@ -0,0 +1,34 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Traki.Api.Contracts.Project;
+using Traki.Api.Handlers;
+
+namespace Traki.Api.Controllers
+{
+    [Route("api/projects")]
+    [ApiController]
+    public class ProjectsController: ControllerBase
+    {
+        private readonly IProjectsHandler _projectsHandler;
+        private readonly IMapper _mapper;
+
+        public ProjectsController(IProjectsHandler projectsHandler, IMapper mapper)
+        {
+            _projectsHandler = projectsHandler;
+            _mapper = mapper;
+        }
+
+        [HttpGet(("{projectId}"))]
+        public async Task<ActionResult<GetProjectResponse>> GetProject(int projectId)
+        {
+            var project = await _projectsHandler.GetProject(projectId);
+
+            if (project == null)
+            {
+                return NotFound();
+            }
+
+            return _mapper.Map<GetProjectResponse>(project);
+        }
+    }
+}
