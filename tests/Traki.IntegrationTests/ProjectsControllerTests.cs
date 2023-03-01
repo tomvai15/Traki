fd97e2d2899c0e85489dbc272eda81bbdfb0de95
tomvai15 <78442825+tomvai15@users.noreply.github.com>
2023-03-01 21:19:49 +0200

    style: fix names of test classes

--- tests/Traki.IntegrationTests/ProjectsControllerTests.cs
+++ tests/Traki.IntegrationTests/ProjectsControllerTests.cs
@@ -2,24 +2,22 @@
 using Microsoft.AspNetCore.Mvc.Testing;
 using Newtonsoft.Json;
 using System.Net;
-using Traki.Api.Contracts.Product;
 using Traki.Api.Contracts.Project;
 using Traki.Api.Data;
 
 namespace Traki.IntegrationTests
 {
-    [Collection("Sequential")]
-    public class ProductsControllerTests : IClassFixture<WebApplicationFactory<Program>>
+    public class ProjectsControllerTests : IClassFixture<WebApplicationFactory<Program>>
     {
         private readonly WebApplicationFactory<Program> _factory;
 
-        public ProductsControllerTests(WebApplicationFactory<Program> factory)
+        public ProjectsControllerTests(WebApplicationFactory<Program> factory)
         {
             _factory = factory;
         }
 
         [Theory]
-        [InlineData("/api/products/1")]
+        [InlineData("/api/projects/1")]
         public async Task Get_ExistingProject_ReturnSuccess(string url)
         {
             // Arrange
@@ -31,17 +29,17 @@ namespace Traki.IntegrationTests
             // Assert
             response.IsSuccessStatusCode.Should().BeTrue();
 
-            var expectedResponse = ExampleData.Products.First();
+            var expectedResponse = ExampleData.Projects.First();
 
             string body = await response.Content.ReadAsStringAsync();
-            GetProductResponse getProductResponse = JsonConvert.DeserializeObject<GetProductResponse>(body);
-            getProductResponse.Product.Should().BeEquivalentTo(expectedResponse, 
-                options => options.Excluding(x => x.Project).Excluding(x=> x.Id));
+            GetProjectResponse getProjectResponse = JsonConvert.DeserializeObject<GetProjectResponse>(body);
+            getProjectResponse.Project.Should().BeEquivalentTo(expectedResponse, 
+                options => options.Excluding(x => x.Products).Excluding(x=> x.Id));
         }
 
         [Theory]
-        [InlineData("/api/products/1")]
-        [InlineData("/api/products")]
+        [InlineData("/api/projects/1")]
+        [InlineData("/api/projects")]
         public async Task Get_ReturnSuccess(string url)
         {
             // Arrange
@@ -55,7 +53,7 @@ namespace Traki.IntegrationTests
         }
 
         [Theory]
-        [InlineData("/api/products/10001")]
+        [InlineData("/api/projects/10001")]
         public async Task Get_NotExistingProject_Return404Response(string url)
         {
             // Arrange
