f869734232400654a5a004f3d38bf05c9ed7d78c
tomvai15 <78442825+tomvai15@users.noreply.github.com>
2023-03-18 09:35:24 +0200

    chore: add docusign nuget package

---
+++ src/Traki.Api/Handlers/DocusignHandler.cs
@@ -0,0 +1,16 @@
+﻿using DocuSign.eSign.Client;
+
+namespace Traki.Api.Handlers
+{
+    public interface IDocusignHandler
+    {
+    }
+
+    public class DocusignHandler
+    {
+        public void CreateDocument()
+        {
+            var docuSignClient = new DocuSignClient("test");
+        }
+    }
+}
