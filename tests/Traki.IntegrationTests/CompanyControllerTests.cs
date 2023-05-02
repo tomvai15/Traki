e5c1e3edc4f7f676d131f381c49368b34d3e5391
tomvai15 <78442825+tomvai15@users.noreply.github.com>
2023-05-02 16:52:24 +0300

    feat: add more integration tests

---
+++ tests/Traki.IntegrationTests/CompanyControllerTests.cs
@@ -0,0 +1,62 @@
+﻿using Azure;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc.Testing;
+using System.Net;
+using Traki.Api.Contracts.Company;
+using Traki.Infrastructure.Data;
+using Traki.IntegrationTests.Extensions;
+
+namespace Traki.IntegrationTests
+{
+    [Collection("Sequential")]
+    public class CompanyControllerTests
+    {
+        private readonly WebApplicationFactory<Program> _factory;
+
+        public CompanyControllerTests(WebApplicationFactory<Program> factory)
+        {
+            _factory = factory;
+        }
+
+        [Fact]
+        public async Task GetCompany_ReturnsCompany()
+        {
+            // Arrange
+            int companyId = 1;
+
+            string uri = $"api/companies/{companyId}";
+            var client = _factory.GetCustomHttpClient();
+            await client.LoginAsAdministrator();
+            var expectedCompany = ExampleData.Companies.First();
+
+            // Act
+            var response = await client.Get<GetCompanyResponse>(uri);
+
+            // Assert
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+            var company = response.Data.Company;
+
+            company.Id.Should().Be(companyId);
+            company.Name.Should().BeEquivalentTo(expectedCompany.Name);
+            company.Address.Should().BeEquivalentTo(expectedCompany.Address);
+        }
+
+        [Fact]
+        public async Task GetCompany_WhenNotAdministrator_ReturnsForbidden()
+        {
+            // Arrange
+            int companyId = 1;
+
+            string uri = $"api/companies/{companyId}";
+            var client = _factory.GetCustomHttpClient();
+            await client.LoginAsProductManager();
+            var expectedCompany = ExampleData.Companies.First();
+
+            // Act
+            var response = await client.Get<GetCompanyResponse>(uri);
+
+            // Assert
+            response.StatusCode.Should().Be(HttpStatusCode.Forbidden);
+        }
+    }
+}
---
+++ tests/Traki.IntegrationTests/DefectControllerTests.cs
@@ -0,0 +1,47 @@
+﻿using Azure;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc.Testing;
+using System.Net;
+using Traki.Api.Contracts.Auth;
+using Traki.Api.Contracts.Drawing.Defect;
+using Traki.Infrastructure.Data;
+using Traki.IntegrationTests.Extensions;
+
+namespace Traki.IntegrationTests
+{
+    [Collection("Sequential")]
+    public class DefectControllerTests
+    {
+        private readonly WebApplicationFactory<Program> _factory;
+        private readonly string baseUri = "api/auth";
+
+        public DefectControllerTests(WebApplicationFactory<Program> factory)
+        {
+            _factory = factory;
+        }
+
+        [Fact]
+        public async Task GetDefect_RetursnDefect()
+        {
+            // Arrange
+            int drawingId = 1;
+            int defectId = 1;
+            string uri = $"api/drawings/{drawingId}/defects/{defectId}";
+            var client = _factory.GetCustomHttpClient();
+            await client.LoginAsProductManager();
+            var expectedDefect = ExampleData.Defects.First();
+
+            // Act
+            var response = await client.Get<GetDefectResponse>(uri);
+
+            // Assert
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+            var defect = response.Data.Defect;
+
+            defect.Id.Should().Be(defectId);
+            defect.Title.Should().BeEquivalentTo(expectedDefect.Title);
+            defect.Description.Should().BeEquivalentTo(expectedDefect.Description);
+            defect.ImageName.Should().BeEquivalentTo(expectedDefect.ImageName);
+        }    
+    }
+}
